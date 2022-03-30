e8785f7fe1a49e4d86e2b2dd7deb7f362969e104
tran0460 <tran0460@algonquinlive.com>
2022-03-29 22:03:51 -0400

    added api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+const api = {
+  key: 'beeff04c2d7634dde414fdaace9bca74',
+  base: 'http://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
